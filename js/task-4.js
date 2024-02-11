0a516bbc297ca8ecb0acda30661eccab7ad39212
Myro-D <133910521+Myro-D@users.noreply.github.com>
2024-02-10 21:56:26 -0500

    +

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,20 @@
 function getShippingCost(country) {
   switch (country) {
     case 'China':
-      console.log(`Shipping to ${country} will cost  100 credits`);
+      return `Shipping to ${country} will cost  100 credits`;
       break;
     case 'Chile':
-      console.log(`Shipping to ${country} will cost  250 credits`);
+      return `Shipping to ${country} will cost  250 credits`;
       break;
 
     case 'Australia':
-      console.log(`Shipping to ${country} will cost  170 credits`);
+      return `Shipping to ${country} will cost  170 credits`;
       break;
     case 'Jamaica':
-      console.log(`Shipping to ${country} will cost  120 credits`);
+      return `Shipping to ${country} will cost  120 credits`;
       break;
     default:
-      console.log('Sorry, there is no delivery to your country');
+      return 'Sorry, there is no delivery to your country';
   }
 }
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
